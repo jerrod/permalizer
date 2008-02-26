94edf98f68d516597a9f6aaaa6676c24a81e5f25
Robert Evans <revans@robertrevans.com>
2008-02-26 01:22:25 -0800

    Converting from test unit (rails plugin) to specs

--- spec/permalizer_spec.rb
+++ spec/permalizer_spec.rb
@@ -1,7 +1,42 @@
 require File.dirname(__FILE__) + '/spec_helper'
+MERB_PERMALIZER_ROOT = File.dirname(__FILE__) + "/.."
+# same structure as merb_helpers
 
 describe "permalizer" do
-  it "should do nothing" do
-    true.should == true
+  
+  def unload_permalizer
+    Merb.class_eval do
+      remove_const("Permalizer") if defined?(Merb::Permalizer)
+    end
   end
+  
+  def reload_permalizer
+    unload_permalizer
+    load(MERB_PERMALIZER_ROOT + "/lib/permalizer.rb")
+  end
+  
+  before(:each) do
+    unload_permalizer
+  end
+  
+  after(:all) do
+    reload_permalizer
+  end
+  
+  it "should not have permalizer" do
+    unload_permalizer
+    defined?(Merb::Permalizer).should be_nil
+  end
+  
+  it "should load permalizer" do
+    unload_permalizer
+    reload_permalizer
+    defined?(Merb::Permalizer).should_not be_nil
+  end
+  
+  it "should load permalizer by default" do
+    reload_permalizer
+    defined?(Merb::Permalizer).should_not be_nil
+  end
+  
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 $TESTING=true
 $:.push File.join(File.dirname(__FILE__), '..', 'lib')
+require 'rubygems'
+require 'merb'
+require 'merb/test/rspec'
+
+Spec::Runner.configure do |config|
+  config.include(Merb::Test::Helper)
+  config.include(Merb::Test::RspecMatchers)
+end
