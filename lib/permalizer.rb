c2c7b0a427787845fe664bab05d5b8d2e1020604
Marcos Arias <markus@agil-e.com>
2008-04-30 19:53:14 +0200

    Refactored. Added a miscelaneous functionality to fix a bug with non-english strings in some Linux machines.

--- lib/permalizer.rb
+++ lib/permalizer.rb
@@ -2,9 +2,9 @@
 require 'iconv'
 
 # permalizer files
-require File.dirname(__FILE__) + '/permalizer/permalink.rb'
+require File.dirname(__FILE__) + '/permalizer/permalink'
 
 # open the String Class and include our additions
 String.class_eval do
-  include Permalink
+  include Permalink::InstanceMethods
 end
--- lib/permalizer/permalink.rb
+++ lib/permalizer/permalink.rb
@@ -1,31 +1,45 @@
 
 # Permalizer adds functionality to the String Object so that clean URLs creation is made simple
 module Permalink
+  module InstanceMethods
+    # permalize! is a destructive method that will make the given string for use as a clean URL
+    # example:
+    # blog.title = "My Cool Gem!"
+    # blog.title.permalize! # => "my-cool-gem"
+    #
+    def permalize!
+      Permalink::Permalizer.new(self).to_s
+    end
 
-  # permalize! is a destructive method that will make the given string for use as a clean URL
-  # example:
-  # blog.title = "My Cool Gem!"
-  # blog.title.permalize! # => "my-cool-gem"
-  #
-  def permalize!
-    permalink!(self)
+    # permalize is the same as permalize! except that it is not a destructive method
+    # it creates a duplicate of the string and returns it as a clean string for URL usage
+    #
+    def permalize
+      string = self.dup
+      Permalink::Permalizer.new(string).to_s
+    end
   end
-
-  # permalize is the same as permalize! except that it is not a destructive method
-  # it creates a duplicate of the string and returns it as a clean string for URL usage
-  #
-  def permalize
-    string = self.dup
-    permalink!(string)
-  end
-
-  private
-
-    # permalink!
+  
+  class Permalizer
+    
+    
+    # This allow to set Permalink::Permalizer.decompose_string = true in a initializer hook
+    class << self
+      attr_accessor :decompose_string
+    end
+    
+    def initialize(word)
+      # only if decompose_string is true and ActiveSupport is loaed in UTF-8 mode (with UTF8Handler as handler class)
+      # This is required for some remote Linux machines, I regulary use it in production environment
+      @word = (Permalizer.decompose_string && word.respond_to?(:chars) && word.chars.respond_to?(:decompose))? word.chars.decompose : word
+    end
+    
+    # permalink
     # Fix unicode characters, regex unwanted characters, split string, thus removing all whitespace, join it, and downcase it                   
     #
     # TODO: Ruby 1.9 string encoding
-    def permalink!(word)
-      (Iconv.new('US-ASCII//TRANSLIT', 'utf-8').iconv word).gsub(/[^\w\s\-\—]/,'').gsub(/[^\w]|[\_]/,' ').split.join('-').downcase  
-    end  
+    def to_s
+      (Iconv.new('US-ASCII//TRANSLIT', 'utf-8').iconv @word).gsub(/[^\w\s\-\—]/,'').gsub(/[^\w]|[\_]/,' ').split.join('-').downcase
+    end
+  end
 end
