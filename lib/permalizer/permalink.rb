d2cc212e71388414e87f23ccc577a56f771ec2af
rrevans <rrevans@Mini-Machine.gateway.2wire.net>
2008-05-11 15:42:10 -0700

    Added some documentation, changed fix_method to read as translate_to so it makes a bit more sense

--- lib/permalizer/permalink.rb
+++ lib/permalizer/permalink.rb
@@ -2,6 +2,7 @@
 # Permalizer adds functionality to the String Object so that clean URLs creation is made simple
 module Permalink
   module InstanceMethods
+    
     # permalize! is a destructive method that will make the given string for use as a clean URL
     # example:
     # blog.title = "My Cool Gem!"
@@ -21,16 +22,21 @@ module Permalink
   
   class Permalizer
     
-    # Class variable which determines the Iconv fix method
-    @@fix_method = :us_ascii
-    # Class variable which determines whether the decompose hook is applied
+    # = Class variable which determines the Iconv fix method
+    # us_ascii is the default
+    @@translate_to = :us_ascii
+    
+    # = Class variable which determines whether the decompose hook is applied
+    # defaults to false
     @@decompose_string = false
     
+
     class << self
       attr_accessor :decompose_string
-      attr_accessor :fix_method
+      attr_accessor :translate_to
     end
     
+
     def initialize(word)
       # only if decompose_string is true and ActiveSupport is loaed in UTF-8 mode (with UTF8Handler as handler class)
       # This is required for some remote Linux machines, I regulary use it in production environment
@@ -38,35 +44,49 @@ module Permalink
       decompose! if Permalizer.decompose_string && word.respond_to?(:chars) && word.chars.respond_to?(:decompose)
     end
     
+    # translate to us_ascii
     def us_ascii
       Iconv.new('US-ASCII//TRANSLIT', 'UTF-8').iconv @word
     end
     
+    # translate to utf-8
     def utf_8
       Iconv.new('UTF-8//TRANSLIT//IGNORE', 'UTF-8').iconv @word
     end
     
+    # translate to iso_8859_1
     def iso_8859_1
       Iconv.new('ISO-8859-1//TRANSLIT//IGNORE', 'UTF-8').iconv @word
     end
     
+    
     def to_s
-      transform send(@@fix_method)
+      transform send(@@translate_to)
     rescue
       transform us_ascii
     end
     
     protected
     
-    def transform(word)
-      word.gsub(/[^\w\s\-\—]/,'').gsub(/[^\w]|[\_]/,' ').split.join('-').downcase
-    end
+      # = Transform
+      # Strips out unnecessary characters from any given word or phrase
+      # 
+      # * first strips non word characters, replacing it with nothing
+      # * it keeps spaces and dashes, instead of replacing them with nothing
+      # * does a second pass of removing non-word characters, replacing it with a space
+      # * removes underscores, replacing it with a space
+      # * then splits the phrase into an array
+      # * rejoins the words with a dash
+      # * then downcases the phrase
+      def transform(word)
+        word.gsub(/[^\w\s\-\—]/,'').gsub(/[^\w]|[\_]/,' ').split.join('-').downcase
+      end
     
     private
     
-    def decompose!
-      @word = @word.chars.decompose
-    end
+      def decompose!
+        @word = @word.chars.decompose
+      end
     
   end
 end
